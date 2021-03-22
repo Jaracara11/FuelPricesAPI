7ca98bb6a8b076d8598fdc5d004c49d6464e536e
Luis Eduardo <undernet1@hotmail.com>
2021-03-22 15:18:10 -0400

    Cambio para retornar todos los combustibles

--- FuelPricesAPI/Controllers/FuelController.cs
+++ FuelPricesAPI/Controllers/FuelController.cs
@@ -12,10 +12,8 @@ namespace FuelPricesAPI.Controllers
     [ApiController]
     public class FuelController : ControllerBase
     {
-        FormatResult fr = new FormatResult();
-
         [HttpGet]
-        public async Task<string> GetGasolinePrices()
+        public async Task<List<string>> GetFuelPrices()
         {
             var client = new HttpClient();
             var response = await client.GetStringAsync(Fuel.UrlFuel);
@@ -24,15 +22,35 @@ namespace FuelPricesAPI.Controllers
 
             htmlDoc.LoadHtml(response);
 
-            HtmlNode[] htmlNode = htmlDoc.DocumentNode.SelectNodes("//tr")
-                .Where(x => x.InnerText.Contains("Gasolina")).ToArray();
+            HtmlNode[] htmlNode = htmlDoc.DocumentNode.SelectNodes("//tbody").ToArray();
 
             foreach (HtmlNode item in htmlNode)
             {
                 resultHtml.Add(item.InnerHtml);
             }
 
-            return fr.JsonPricesGasoline(resultHtml);
+            return resultHtml;
+        }
+
+        [Route("{fuel}")]
+        [HttpPost]
+        public async Task<string> GetFuelPrice(string fuel)
+        {
+            var client = new HttpClient();
+            var response = await client.GetStringAsync(Fuel.UrlFuel);
+            var htmlDoc = new HtmlDocument();
+            var resultHtml = new List<string>();
+
+            htmlDoc.LoadHtml(response);
+
+            HtmlNode[] htmlNode = htmlDoc.DocumentNode.SelectNodes("//tbody").ToArray();
+
+            foreach (HtmlNode item in htmlNode)
+            {
+                resultHtml.Add(item.InnerHtml);
+            }
+
+            return resultHtml;
         }
     }
 }
--- FuelPricesAPI/Models/Fuel.cs
+++ FuelPricesAPI/Models/Fuel.cs
@@ -1,15 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace FuelPricesAPI.Models
 {
     public class Fuel
     {
         public static string UrlFuel = "https://www.micm.gob.do/direcciones/combustibles";
-
-        public string FuelPremium { get; set; }
-
-        public string FuelRegular { get; set; }
-
     }
 
 }
